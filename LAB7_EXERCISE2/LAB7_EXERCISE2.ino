f5b8fbdecd9d19658d5d028aa350e189682166c0
Jiratchaya1126 <60213984+Jiratchaya1126@users.noreply.github.com>
2020-02-27 15:40:21 +0700

    LAB7

---
+++ LAB7_EXERCISE2/LAB7_EXERCISE2.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+#define OLED_RESET -1 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#include <ThreeWire.h>  
+#include <RtcDS1302.h>
+ThreeWire myWire(6,7,8); // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
+
+void displayDateTime(const RtcDateTime& dt){
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print(dt.Year(), DEC);
+  display.print('/');
+  display.print(dt.Month(), DEC); 
+  display.print('/'); 
+  display.print(dt.Day(), DEC); 
+  display.println();
+  display.println(); 
+  display.print(dt.Hour(), DEC);
+  display.print(':'); 
+  display.print(dt.Minute(), DEC);
+  display.print(':'); 
+  display.print(dt.Second(), DEC);
+  display.display();
+}
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+  display.clearDisplay();
+  display.drawPixel(127, 63, WHITE);
+
+  Rtc.Begin();
+  RtcDateTime setting_time(2020,2,27,15,37,00);
+  Rtc.SetDateTime(setting_time);
+  }
+  
+  void loop() {
+    RtcDateTime now = Rtc.GetDateTime();
+    displayDateTime(now);
+}
