33dd906ccdf5ec61d87be53cf1bdb402dfffe54f
Jiratchaya1126 <60213984+Jiratchaya1126@users.noreply.github.com>
2020-03-19 14:45:46 +0700

    LAB9

---
+++ EXERCISE1_LAB9/EXERCISE1_LAB9.ino
@@ -0,0 +1,29 @@
+const byte LED_pin = 8;
+const byte Interrupt_pin = 2;
+volatile byte state = LOW;// due to RISING is LOW to HIGH
+
+int i=1;
+int a = 1000;
+
+void setup()
+{
+  pinMode(LED_pin, OUTPUT);
+  pinMode(Interrupt_pin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(Interrupt_pin), blink, RISING);
+}
+
+void loop()
+{
+  
+  digitalWrite(LED_pin, state);
+  delay(a);// "a" from loop
+  state = !state;//LOW to HIGH
+}
+
+void blink()
+{
+  if (a>3000)
+   a=1000;
+   a+=1000;
+  
+}
